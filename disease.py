9053f5a56efb7f0019e4eb9ad0b27cf3b816e13b
yourscoder <98487829+yourscoder@users.noreply.github.com>
2026-01-11 21:01:58 +0600

    Import libraries and load dataset from file
    Added necessary imports and dataset loading functionality.

--- disease.py
+++ disease.py
@@ -1,1 +1,21 @@
+import pandas as pd
+import numpy as np
+import torch
+import spacy
+import re
 
+from google.colab import files
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+from transformers import (
+    DistilBertTokenizerFast,
+    DistilBertForSequenceClassification,
+    Trainer,
+    TrainingArguments
+)
+
+"""# dataset insertion"""
+
+uploaded = files.upload()
+df = pd.read_csv(list(uploaded.keys())[0])
